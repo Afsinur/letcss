4b5d820bfadcfd689f750699cdf5804c7941aa3a
afsinur <afsin.sayem1@gmail.com>
2024-02-06 22:42:50 +0600

    v5 | mutation update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 setTimeout(() => {
-  document.querySelectorAll("p").forEach((p) => {
-    p.classList.add("let('p','text-white,font-semibold,text-3xl')");
-  });
+  document.querySelector("[data-app-2]").classList.add("set('p1,p2')");
+
+  setTimeout(() => {
+    document.querySelector("[data-app-2]").classList.remove("set('p1,p2')");
+
+    setTimeout(() => {
+      document.querySelector(
+        "[data-app]"
+      ).innerHTML = `<div class="let('p3','bg-blue-500')">js <div class="set('p2')">css</div></div>`;
+    }, 5000);
+  }, 1000);
 }, 2000);
--- test/letcss.js
+++ test/letcss.js
@@ -1,6 +1,7 @@
 let varsArr = [];
 let itmG = null;
 let setterPrefixGlobal = null;
+let variablePrefixGlobal = null;
 
 function let(...params) {
   let [vars, str] = params;
@@ -10,16 +11,16 @@ function let(...params) {
       let classes = str.split(",");
 
       itmG.classList.add(...classes);
-      varsArr.push({ vars, classes });
+      varsArr.push({ vars, classes, itm: itmG });
     }
   } else {
     if (!varsArr.some((obj) => obj.vars == vars)) {
-      let classes = Array.from(str).filter(
+      let classes = Array.from(str.lastItmClas).filter(
         (itm) => !itm.includes(setterPrefixGlobal)
       );
 
       itmG.classList.add(...classes);
-      varsArr.push({ vars, classes });
+      varsArr.push({ vars, classes, itm: itmG });
     }
   }
 }
@@ -39,7 +40,7 @@ function set(...params) {
     }
   });
 
-  return itmG.classList;
+  return { varNames: varName.split(","), lastItmClas: itmG.classList };
 }
 function add(str) {
   let clsArr = str.split(",");
@@ -51,66 +52,106 @@ function remove(str) {
 
   return { fnName: "remove", clsArr };
 }
+function setItmGlobalCreateFunctionAndRemoveClass(cls, itm) {
+  itmG = itm;
 
-window.addEventListener("DOMContentLoaded", () => {
-  function setItmGlobalCreateFunctionAndRemoveClass(cls, itm) {
-    itmG = itm;
-
-    try {
-      let setVarsClass = new Function(cls);
-      setVarsClass();
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    let setVarsClass = new Function(cls);
+    setVarsClass();
+  } catch (error) {
+    console.log(error);
   }
+}
+function loadLetCss(setterPrefix = "let", variablePrefix = "set") {
+  setterPrefixGlobal = setterPrefix;
+  variablePrefixGlobal = variablePrefix;
+  let allElement = document.querySelectorAll("*");
 
-  function loadLetCss(setterPrefix = "let", variablePrefix = "set") {
-    setterPrefixGlobal = setterPrefix;
-    let allElement = document.querySelectorAll("*");
-
-    //let()
-    allElement.forEach((itm) => {
-      Array.from(itm.classList).forEach((cls) => {
-        if (cls.includes(setterPrefix)) {
-          setItmGlobalCreateFunctionAndRemoveClass(cls, itm);
-        }
-      });
+  //let()
+  allElement.forEach((itm) => {
+    Array.from(itm.classList).forEach((cls) => {
+      if (cls.includes(setterPrefix)) {
+        setItmGlobalCreateFunctionAndRemoveClass(cls, itm);
+      }
     });
-    //set()
-    allElement.forEach((itm) => {
-      Array.from(itm.classList).forEach((cls) => {
-        if (cls.includes(variablePrefix)) {
-          setItmGlobalCreateFunctionAndRemoveClass(cls, itm);
-        }
-      });
+  });
+  //set()
+  allElement.forEach((itm) => {
+    Array.from(itm.classList).forEach((cls) => {
+      if (cls.includes(variablePrefix)) {
+        setItmGlobalCreateFunctionAndRemoveClass(cls, itm);
+      }
     });
-  }
+  });
+}
+function watchForAnyChanges() {
+  const targetElement = document.querySelectorAll("*");
 
-  function watchForAnyChanges() {
-    const targetElement = document.querySelectorAll("*");
-    const onClassAdded = (mutationsList, observer) => {
-      for (const mutation of mutationsList) {
-        if (
-          mutation.type === "attributes" &&
-          mutation.attributeName === "class"
-        ) {
-          observer.disconnect();
-          loadLetCss();
-          startMe();
+  const onClassAdded = (mutationsList, observer) => {
+    for (const mutation of mutationsList) {
+      if (
+        mutation.type === "attributes" &&
+        mutation.attributeName === "class"
+      ) {
+        console.log(
+          mutation.oldValue
+          //mutation.target
+          //mutation.target.classList,
+          //previousClasses
+        );
+
+        if (mutation.oldValue) {
+          if (
+            mutation.oldValue
+              .split(" ")
+              .filter((itm) => itm.includes(variablePrefixGlobal)).length > 0
+          ) {
+            let str = mutation.oldValue
+              .split(" ")
+              .filter((itm) => itm.includes(variablePrefixGlobal))[0];
+            let fn = new Function(`return ${str}`);
+
+            itmG = mutation.target;
+            fn().varNames.forEach((vr) => {
+              let fn = new Function(
+                `set('${vr}',remove('${varsArr
+                  .filter((obj) => obj.vars == vr)[0]
+                  .classes.join(",")}'))`
+              );
+              fn();
+            });
+          }
         }
       }
-    };
-    const observer = new MutationObserver(onClassAdded);
-    const config = { attributes: true, attributeFilter: ["class"] };
-    const startMe = () => {
-      targetElement.forEach((itm) => {
-        observer.observe(itm, config);
-      });
-    };
 
-    startMe();
-  }
+      observer.disconnect();
+      loadLetCss();
+      startMe();
+    }
+  };
+  const observer = new MutationObserver(onClassAdded);
+  //const config = { attributes: true, attributeFilter: ["class"] };
+  const config = {
+    childList: true,
+    attributes: true,
+    attributeOldValue: true,
+    attributeFilter: ["class"],
+    subtree: true,
+  };
+  const startMe = () => {
+    targetElement.forEach((itm) => {
+      observer.observe(itm, config);
+    });
+  };
 
+  startMe();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
   loadLetCss();
   watchForAnyChanges();
 });
+
+// window.addEventListener("DOMSubtreeModified", () => {
+//   loadLetCss();
+// });
